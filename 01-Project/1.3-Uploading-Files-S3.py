397be2d92fc538e953f4fca73f86fa112c360af3
faizan35 <faizanansari4862@gmail.com>
2024-07-04 18:27:04 +0530

    lines added

--- 01-Project/1.3-Uploading-Files-S3.py
+++ 01-Project/1.3-Uploading-Files-S3.py
@@ -16,19 +16,32 @@
 # s3.upload_file('./Readme.md', 'mm-yy', 'Readme.md')
     
 
+#########################################################################
+
 # 2. Modify the script to upload multiple files from a directory.
+
+
+# import boto3
+# import os
+
+# s3 = boto3.client('s3')
+
+# files_in_dir = os.listdir(r'/home/vagrant/BOTO3_Project_HUB/01-Project')
+
+# for each_file in files_in_dir:
+#     s3.upload_file(each_file, 'yo-yo-bucket', each_file)
+
+
+
+#########################################################################
+
+
+# 3. Add error handling for common issues like missing files or incorrect permissions.
 
 
 import boto3
 import os
 
 s3 = boto3.client('s3')
-
-files_in_dir = os.listdir(r'/home/vagrant/BOTO3_Project_HUB/01-Project')
-
-for each_file in files_in_dir:
-    s3.upload_file(each_file, 'yo-yo-bucket', each_file)
-
-
 
 
