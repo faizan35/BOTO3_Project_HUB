fa1950504c3f312752ff73336b4027b2299225d0
faizan35 <faizanansari4862@gmail.com>
2024-07-04 18:20:20 +0530

    1.3. task 2

--- 01-Project/1.3-Uploading-Files-S3.py
+++ 01-Project/1.3-Uploading-Files-S3.py
@@ -9,12 +9,25 @@
 
 # 1. Write a script to upload a single file to an S3 bucket.
 
+# import boto3
+
+# s3 = boto3.client('s3')
+
+# s3.upload_file('./Readme.md', 'mm-yy', 'Readme.md')
+    
+
+# 2. Modify the script to upload multiple files from a directory.
+
+
 import boto3
+import os
 
 s3 = boto3.client('s3')
 
-s3.upload_file('./Readme.md', 'mm-yy', 'Readme.md')
-    
+files_in_dir = os.listdir(r'/home/vagrant/BOTO3_Project_HUB/01-Project')
+
+for each_file in files_in_dir:
+    s3.upload_file(each_file, 'yo-yo-bucket', each_file)
 
 
 
